32717df399ae39e8b40d9654ea008edfff4317a3
Gustav Eikaas <guei@equinor.com>
2024-01-29 19:24:25 +0100

    initial aliases

---
+++ git.ps1
@@ -0,0 +1,22 @@
+
+# git pull alias
+function gitp {
+  git pull 
+}
+
+# git add alias
+function gita {
+  & git add $args
+}
+
+# git commit alias
+function gitc {
+  & git commit $args
+}
+
+# git reset hard 
+function gitr {
+  git reset --hard origin/main
+  git clean -f -d
+}
+
