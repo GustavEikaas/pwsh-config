8f036dfd59ec01492f2b6f81c6a88194161c217b
Gustav Eikaas <guei@equinor.com>
2024-01-29 20:12:32 +0100

    add helper

--- git.ps1
+++ git.ps1
@@ -34,4 +34,16 @@ function ggpush {
     git push $args
 
     Write-Host "Files changed: $filesChangedBefore, Commits pushed: $commitsAheadBefore"
-} 
+}
+
+
+
+function ggh {
+    Write-Host "Available Git Helper Commands:"
+    Write-Host "  ggp  - Git Pull: Pull changes from the remote repository."
+    Write-Host "  gga  - Git Add: Add changes to the staging area."
+    Write-Host "  ggc  - Git Commit: Commit changes with a custom message."
+    Write-Host "  ggs  - Git Status: Show the status of the working directory."
+    Write-Host "  ggr  - Git Reset Hard: Reset to the latest commit on the remote main branch."
+    Write-Host "  ggpush - Git Push: Push changes to the remote repository with information about files changed and commits pushed."
+}
