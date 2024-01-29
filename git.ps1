14f45513845f1fe58c286b5543458ed784961f60
Gustav Eikaas <guei@equinor.com>
2024-01-29 20:15:32 +0100

    chore

--- git.ps1
+++ git.ps1
@@ -46,14 +46,15 @@ function ggpush {
     Write-Host "Files changed: $filesChangedBefore, Commits pushed: $commitsAheadBefore"
 }
 
-
-
 function ggh {
     Write-Host "Available Git Helper Commands:"
-    Write-Host "  ggp  - Git Pull: Pull changes from the remote repository."
-    Write-Host "  gga  - Git Add: Add changes to the staging area."
-    Write-Host "  ggc  - Git Commit: Commit changes with a custom message."
-    Write-Host "  ggs  - Git Status: Show the status of the working directory."
-    Write-Host "  ggr  - Git Reset Hard: Reset to the latest commit on the remote main branch."
-    Write-Host "  ggpush - Git Push: Push changes to the remote repository with information about files changed and commits pushed."
+    Write-Host "  ggp     - Git Pull: Pull changes from the remote repository."
+    Write-Host "  gga     - Git Add: Add changes to the staging area."
+    Write-Host "  ggc     - Git Commit: Commit changes with a custom message."
+    Write-Host "  ggs     - Git Status: Show the status of the working directory."
+    Write-Host "  ggch    - Git Checkout: Switch to a different branch or commit."
+    Write-Host "  ggr     - Git Reset Hard: Reset to the latest commit on the remote main branch."
+    Write-Host "  ggrmain - Git Reset Hard Main: Reset to the latest commit on the remote main branch, checkout main, and pull changes."
+    Write-Host "  ggpush  - Git Push: Push changes to the remote repository with information about files changed and commits pushed."
 }
+
