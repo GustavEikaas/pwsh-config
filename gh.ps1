2688a59568e6e4badcd07821a74c66bd56ea045a
Gustav Eikaas <gustav.eikaas@gmail.com>
2024-02-21 17:14:33 +0100

    add merge question

--- gh.ps1
+++ gh.ps1
@@ -12,8 +12,19 @@ function ghprc{
     gh pr view
     ggrm
 #>
-function ghprm{
-  gh pr merge --squash 
+function ghprm {
+  $mergeOutput = gh pr merge --squash 
+  if($mergeOutput -match "GraphQL: Pull Request is still a draft"){
+    $markReady = ask -Question "Pull Request still draft, mark as ready and merge? (Y to merge, N to cancel)"
+    if($markReady){
+      gh pr ready
+    }else{
+      Write-Host "Aborting..."
+      return
+    }
+  }
+
+  ghprm
   ghprv
   ggrm
 }
@@ -40,4 +51,32 @@ function ghprd{
 #>
 function ghprv{
   gh pr view $args     
+}
+
+function ask{
+    param (
+        [string]$Question
+    )
+
+    # Display the question
+    Write-Host $Question -ForegroundColor Cyan
+
+    # Prompt for user input
+    $response = Read-Host "(Y/N)"
+
+    # Convert the response to uppercase for case-insensitive comparison
+    $response = $response.ToUpper()
+
+    # Check if the response is valid
+    if ($response -eq 'Y' -or $response -eq 'YES') {
+        return $true
+    }
+    elseif ($response -eq 'N' -or $response -eq 'NO') {
+        return $false
+    }
+    else {
+        Write-Host "Invalid response. Please enter Y or N." -ForegroundColor Red
+        # Recursively call the function to prompt again
+        return (ask -Question $Question)
+    }
 }
