a518557d4cb4e7d2937407cb3fe9d83fd7ae9fb0
Gustav Eikaas <gustav.eikaas@gmail.com>
2024-04-06 22:14:54 +0200

    add spotify

---
+++ spotify.ps1
@@ -0,0 +1,23 @@
+<#
+.SYNOPSIS
+    spt playback --next
+#>
+function ssn{
+  spt playback --next
+}
+
+<#
+.SYNOPSIS
+    spt playback --next
+#>
+function sst{
+  spt playback --toggle
+}
+
+<#
+.SYNOPSIS
+    spt playback --next
+#>
+function ssp{
+  spt playback --previous
+}
